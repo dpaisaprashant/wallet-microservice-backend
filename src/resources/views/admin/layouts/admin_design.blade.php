231bcb5641646f0e3794d6c76386bb120f04a588
Avaya Baniya <baniyaavaya@gmail.com>
2021-04-25 11:54:40 +0545

    asset to secure asset

--- src/resources/views/admin/layouts/admin_design.blade.php
+++ src/resources/views/admin/layouts/admin_design.blade.php
@@ -10,11 +10,11 @@
 
     <title>ICash Admin Panel</title>
 
-    <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('admin/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('admin/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('admin/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('admin/css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('admin/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('admin/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('admin/css/style.css') }}" rel="stylesheet">
 
     <style>
 
@@ -106,15 +106,15 @@
 </div>
 
 <!-- Mainly scripts -->
-<script src="{{ asset('admin/js/jquery-3.1.1.min.js') }} " ></script>
-<script src="{{ asset('admin/js/popper.min.js') }} " ></script>
-<script src="{{ asset('admin/js/bootstrap.js') }} " ></script>
-<script src="{{ asset('admin/js/plugins/metisMenu/jquery.metisMenu.js') }} " ></script>
-<script src="{{ asset('admin/js/plugins/slimscroll/jquery.slimscroll.min.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/jquery-3.1.1.min.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/popper.min.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/bootstrap.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/plugins/metisMenu/jquery.metisMenu.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/plugins/slimscroll/jquery.slimscroll.min.js') }} " ></script>
 
 <!-- Custom and plugin javascript -->
-<script src="{{ asset('admin/js/inspinia.js') }} " ></script>
-<script src="{{ asset('admin/js/plugins/pace/pace.min.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/inspinia.js') }} " ></script>
+<script src="{{ secure_asset('admin/js/plugins/pace/pace.min.js') }} " ></script>
 
 <script>
     $('form').attr('autocomplete','off');

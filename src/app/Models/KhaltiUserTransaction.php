6cd832630f3bf9d350e1ca2bbb4fe7528da4e89d
Avaya Baniya <baniyaavaya@gmail.com>
2021-06-02 15:56:47 +0545

    add khalti user transaction

---
+++ src/app/Models/KhaltiUserTransaction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\BelongsToUser;
+use App\Traits\BelongsToUseThroughMicroservice;
+use App\Traits\MorphOneCommission;
+use App\Traits\MorphOneDispute;
+use Illuminate\Database\Eloquent\Model;
+
+class KhaltiUserTransaction extends Model
+{
+    use BelongsToUseThroughMicroservice, BelongsToUser, MorphOneCommission, MorphOneDispute;
+
+    //protected $connection = 'paypoint';
+
+    /**
+     * @param $amount
+     * @return float|int
+     */
+    public function getAmountAttribute($amount)
+    {
+        return ($amount/100);
+    }
+
+    protected $casts = [
+        "amount" => "number"
+    ];
+
+    public function transactions()
+    {
+        return $this->morphOne(TransactionEvent::class, 'transactionable','transaction_type', 'transaction_id');
+    }
+}

34e082353ed96788fe11625d1fed288dffafee4f
Rahul Shakya <rahulshakya123rs@gmail.com>
2021-07-27 16:49:35 +0545

    User to bfi fund transfer all & filters also finished

---
+++ src/app/Filters/UserToBFIFundTransferFilter/BFIIdFilter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filters\UserToBFIFundTransferFilter;
+
+use App\Filters\FilterAbstract;
+use Illuminate\Database\Eloquent\Builder;
+
+class BFIIdFilter extends FilterAbstract {
+
+    public function mapping()
+    {
+        return [
+
+        ];
+    }
+
+
+    /**
+     * Apply filter.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     *
+     * @return Builder
+     */
+    public function filter(Builder $builder, $value)
+    {
+        //$value = $this->resolveFilterValue($this->mapping(), $value);
+        if ($value === null) {
+            return $builder;
+        }
+
+        return $builder->where('bfi_id', $value);
+    }
+}
--- src/app/Models/UserToBfiFundTransfer.php
+++ src/app/Models/UserToBfiFundTransfer.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use App\Filters\FiltersAbstract;
+use App\Filters\UserToBFIFundTransferFilter\UserToBFIFundTransferFilters;
 use App\Models\BFI\BFIUser;
 use App\Models\Merchant\MerchantBFI;
 use App\Traits\BelongsToUser;
 use App\Traits\MorphOneTransaction;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class UserToBfiFundTransfer extends Model
 {
-    use BelongsToUser, MorphOneTransaction;
+    use MorphOneTransaction;
 
     protected $connection = "bfi";
 
@@ -24,6 +28,16 @@ class UserToBfiFundTransfer extends Model
 
     public function bfiCheckPayment(){
         return $this->belongsTo(BfiGatewayCheckPayment::class,'process_id','process_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'wallet_id', 'mobile_no');
+    }
+
+    public function scopeFilter(Builder $builder, Request $request, array $filters = [])
+    {
+        return (new UserToBFIFundTransferFilters($request))->add($filters)->filter($builder);
     }
 
 

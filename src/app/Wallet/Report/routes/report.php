676c19017114d8f3b3236f01760a9ffe752fe558
shakyarahul <rahulshakya123rs@gmail.com>
2021-08-17 17:21:34 +0545

    Single user reconciliation report

--- src/app/Wallet/Report/routes/report.php
+++ src/app/Wallet/Report/routes/report.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'admin/report', 'middleware' => ['web','auth']], function () {
 
     Route::match(['get', 'post'],'reconciliation-report', [WalletReportController::class, 'reconciliationReport'])->name('report.reconciliation')->middleware('permission:Report reconciliation');
+
+
     Route::match(['get', 'post'],'users-reconciliation-report', [UserWalletReportController::class, 'userReconciliationReport'])->name('report.user.reconciliation');
 
     Route::match(['get', 'post'],'customer-activity-report', [WalletReportController::class, 'customerActivityReport'])->name('report.clientActivity');

a5141b09d9805c61435bc4d75a933987ba2d54c5
subhay99 <sthapitsubhay99@gmail.com>
2021-08-23 13:22:18 +0545

    api

--- src/resources/views/admin/transaction/preTransaction/status.blade.php
+++ src/resources/views/admin/transaction/preTransaction/status.blade.php
@@ -1,17 +1,26 @@
+{{--@if(strtoupper($transaction->status) == 'SUCCESS')--}}
+{{--    @if(isset($transaction->transactionEvent))--}}
+{{--        @if($transaction->transactionEvent->refundTransaction)--}}
+{{--            <span class="badge badge-inverse">{{ strtoupper("Success Tran. DISPUTE & REFUNDED") }}</span>--}}
+{{--        @else--}}
+{{--            <span class="badge badge-primary">{{ strtoupper($transaction->status) }}</span>--}}
+{{--        @endif--}}
+{{--    @endif--}}
+{{--@elseif(strtoupper($transaction->status) == 'FAILED')--}}
+{{--    @if(isset($transaction->refundTransaction))--}}
+{{--        <span class="badge badge-inverse">{{ strtoupper("Failed Tran. DISPUTE & REFUNDED") }}</span>--}}
+{{--    @else--}}
+{{--        <span class="badge badge-danger">{{ strtoupper($transaction->status) }}</span>--}}
+{{--    @endif--}}
+{{--@else--}}
+{{--    <span class="badge badge-warning">NO RESPONSE</span>--}}
+{{--@endif--}}
+
+
 @if(strtoupper($transaction->status) == 'SUCCESS')
-    @if(isset($transaction->transactionEvent))
-        @if($transaction->transactionEvent->refundTransaction)
-            <span class="badge badge-inverse">{{ strtoupper("Success Tran. DISPUTE & REFUNDED") }}</span>
-        @else
-            <span class="badge badge-primary">{{ strtoupper($transaction->status) }}</span>
-        @endif
-    @endif
+    <span class="badge badge-primary">{{ strtoupper($transaction->status) }}</span>
 @elseif(strtoupper($transaction->status) == 'FAILED')
-    @if(isset($transaction->refundTransaction))
-        <span class="badge badge-inverse">{{ strtoupper("Failed Tran. DISPUTE & REFUNDED") }}</span>
-    @else
-        <span class="badge badge-danger">{{ strtoupper($transaction->status) }}</span>
-    @endif
+    <span class="badge badge-danger">{{ strtoupper($transaction->status) }}</span>
 @else
     <span class="badge badge-warning">NO RESPONSE</span>
 @endif

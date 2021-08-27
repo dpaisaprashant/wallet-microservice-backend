c67a2853b0c3352e9ae446c1cc4e712715470950
Shashwot <shashwot.shakya9@gmail.com>
2021-08-27 14:06:22 +0545

    active_inactive fix

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('http');
+        URL::forceScheme('https');
         UserKYC::observe(AcceptKYCUserKYCObserver::class);
     }
 }

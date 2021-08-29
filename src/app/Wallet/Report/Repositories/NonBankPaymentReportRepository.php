d84522067ee3176cf41de0b3690da6342ac13403
shakyarahul <rahulshakya123rs@gmail.com>
2021-08-29 14:05:38 +0545

    Agent Excluded from the report and agent report fixed

--- src/app/Wallet/Report/Repositories/NonBankPaymentReportRepository.php
+++ src/app/Wallet/Report/Repositories/NonBankPaymentReportRepository.php
@@ -23,101 +23,274 @@ use Illuminate\Http\Request;
 class NonBankPaymentReportRepository extends AbstractReportRepository
 {
 
-    public function getBillPaymentNumber(){
-        $billPaymentTotalNumber = TransactionEvent::whereIn('transaction_type',$this->billPayment)
+    public function getBillPaymentNumber()
+    {
+        $billPaymentTotalNumber = TransactionEvent::whereIn('transaction_type', $this->billPayment)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                    return $query->whereHas('agent', function ($query) {
+                        return $query->where('status', '!=', 'ACCEPTED');
+                    });
+                });
+            });
+        })
             ->filter($this->request)
             ->count();
         return $billPaymentTotalNumber;
     }
 
-    public function getBillPaymentValue(){
-        $billPaymentTotalValue = TransactionEvent::whereIn('transaction_type',$this->billPayment)
+    public function getBillPaymentValue()
+    {
+        /*dd(TransactionEvent::whereIn('transaction_type',$this->billPayment)->filter($this->request)->sum('amount'));*/
+        $billPaymentTotalValue = TransactionEvent::whereIn('transaction_type', $this->billPayment)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->sum('amount');
         return $billPaymentTotalValue;
     }
 
-    public function getTransferNumber(){
-        $transferTotalNumber = TransactionEvent::where('transaction_type',UserToUserFundTransfer::class)
-            ->where('vendor','Transfer Fund')
+    public function getTransferNumber()
+    {
+        $transferTotalNumber = TransactionEvent::where('transaction_type', UserToUserFundTransfer::class)
+            ->whereHas('user', function ($query) {
+
+                return $query->where(function($query){
+                    return $query->doesntHave('agent')
+                        ->orWhere(function ($query) {
+                            return $query->whereHas('agent', function ($query) {
+                                return $query->where('status', '!=', 'ACCEPTED');
+                            });
+                        });
+                });
+            })
             ->filter($this->request)
             ->count();
-        $fundTotalRequestNumber = TransactionEvent::where('transaction_type',FundRequest::class)
+        $fundTotalRequestNumber = TransactionEvent::where('transaction_type', FundRequest::class)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->count();
         return $transferTotalNumber + $fundTotalRequestNumber;
     }
 
-    public function getTransferValue(){
-        $transferTotalValue = TransactionEvent::where('transaction_type',UserToUserFundTransfer::class)
-            ->where('vendor','Transfer Fund')
+    public function getTransferValue()
+    {
+        $transferTotalValue = TransactionEvent::where('transaction_type', UserToUserFundTransfer::class)
+            ->where('vendor', 'Transfer Fund')->whereHas('user', function ($query) {
+
+                return $query->where(function($query){
+                    return $query->doesntHave('agent')
+                        ->orWhere(function ($query) {
+                            return $query->whereHas('agent', function ($query) {
+                                return $query->where('status', '!=', 'ACCEPTED');
+                            });
+                        });
+                });
+            })
             ->filter($this->request)
             ->sum('amount');
-        $fundTotalRequestValue = TransactionEvent::where('transaction_type',FundRequest::class)
-            ->filter($this->request)
+        $fundTotalRequestValue = TransactionEvent::where('transaction_type', FundRequest::class)
+            ->filter($this->request)->whereHas('user', function ($query) {
+
+                return $query->where(function($query){
+                    return $query->doesntHave('agent')
+                        ->orWhere(function ($query) {
+                            return $query->whereHas('agent', function ($query) {
+                                return $query->where('status', '!=', 'ACCEPTED');
+                            });
+                        });
+                });
+            })
             ->sum('amount');
         return $transferTotalValue + $fundTotalRequestValue;
     }
 
-    public function getCashInNumber(){
-        $cashInTotalNumber = TransactionEvent::whereIn('transaction_type',$this->cashIn)
+    public function getCashInNumber()
+    {
+        $cashInTotalNumber = TransactionEvent::whereIn('transaction_type', $this->cashIn)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->count();
         return $cashInTotalNumber;
     }
 
-    public function getCashInValue(){
-        $cashInTotalValue = TransactionEvent::whereIn('transaction_type',$this->cashIn)
+    public function getCashInValue()
+    {
+        $cashInTotalValue = TransactionEvent::whereIn('transaction_type', $this->cashIn)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->sum('amount');
         return $cashInTotalValue;
     }
 
-    public function getOfferNumber(){
-        $cashBackOfferTotalNumber = TransactionEvent::where('service_type','CASHBACK')->where('transaction_type',Commission::class)
+    public function getOfferNumber()
+    {
+        $cashBackOfferTotalNumber = TransactionEvent::where('service_type', 'CASHBACK')->where('transaction_type', Commission::class)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->count();
-        $referralTotalNumber = TransactionEvent::whereIn('transaction_type',[UsedUserReferral::class,UserReferralBonusTransaction::class])
+        $referralTotalNumber = TransactionEvent::whereIn('transaction_type', [UsedUserReferral::class, UserReferralBonusTransaction::class])->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->count();
         return $cashBackOfferTotalNumber + $referralTotalNumber;
     }
 
-    public function getOfferValue(){
-        $cashBackOfferTotalValue = TransactionEvent::where('service_type','CASHBACK')
-            ->where('transaction_type',Commission::class)
+    public function getOfferValue()
+    {
+        $cashBackOfferTotalValue = TransactionEvent::where('service_type', 'CASHBACK')
+            ->where('transaction_type', Commission::class)->whereHas('user', function ($query) {
+
+                return $query->where(function($query){
+                    return $query->doesntHave('agent')
+                        ->orWhere(function ($query) {
+                            return $query->whereHas('agent', function ($query) {
+                                return $query->where('status', '!=', 'ACCEPTED');
+                            });
+                        });
+                });
+            })
             ->filter($this->request)
             ->sum('amount');
-        $referralTotalValue = TransactionEvent::whereIn('transaction_type',[UsedUserReferral::class,UserReferralBonusTransaction::class])
+        $referralTotalValue = TransactionEvent::whereIn('transaction_type', [UsedUserReferral::class, UserReferralBonusTransaction::class])->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->sum('amount');
         return $cashBackOfferTotalValue + $referralTotalValue;
     }
 
-    public function getFeesChargesNumber(){
-        $commissionTotalNumber = TransactionEvent::where('service_type','COMMISSION')
-            ->where('transaction_type',Commission::class)
+    public function getFeesChargesNumber()
+    {
+        $commissionTotalNumber = TransactionEvent::where('service_type', 'COMMISSION')
+            ->where('transaction_type', Commission::class)->whereHas('user', function ($query) {
+
+                return $query->where(function($query){
+                    return $query->doesntHave('agent')
+                        ->orWhere(function ($query) {
+                            return $query->whereHas('agent', function ($query) {
+                                return $query->where('status', '!=', 'ACCEPTED');
+                            });
+                        });
+                });
+            })
             ->filter($this->request)
             ->count();
         return $commissionTotalNumber;
     }
 
-    public function getFeesChargesValue(){
-        $commissionTotalValue = TransactionEvent::where('service_type','COMMISSION')->where('transaction_type',Commission::class)
+    public function getFeesChargesValue()
+    {
+        $commissionTotalValue = TransactionEvent::where('service_type', 'COMMISSION')->where('transaction_type', Commission::class)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->sum('amount');
         return $commissionTotalValue;
     }
 
-    public function getCashOutNumber(){
-        $cashOutTotalNumber = TransactionEvent::where('transaction_type',NchlBankTransfer::class)
+    public function getCashOutNumber()
+    {
+        $cashOutTotalNumber = TransactionEvent::where('transaction_type', NchlBankTransfer::class)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->count();
         return $cashOutTotalNumber;
     }
 
-    public function getCashOutValue(){
-        $cashOutTotalValue = TransactionEvent::where('transaction_type',NchlBankTransfer::class)
+    public function getCashOutValue()
+    {
+        $cashOutTotalValue = TransactionEvent::where('transaction_type', NchlBankTransfer::class)->whereHas('user', function ($query) {
+
+            return $query->where(function($query){
+                return $query->doesntHave('agent')
+                    ->orWhere(function ($query) {
+                        return $query->whereHas('agent', function ($query) {
+                            return $query->where('status', '!=', 'ACCEPTED');
+                        });
+                    });
+            });
+        })
             ->filter($this->request)
             ->sum('amount');
         return $cashOutTotalValue;

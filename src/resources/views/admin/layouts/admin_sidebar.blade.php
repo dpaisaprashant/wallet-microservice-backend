2c797d4352d4b2c2ea2fdeb9a318afbc8df3e3d1
Shashwot <shashwot.shakya9@gmail.com>
2021-09-09 13:14:32 +0545

    paypoint api fix

--- src/resources/views/admin/layouts/admin_sidebar.blade.php
+++ src/resources/views/admin/layouts/admin_sidebar.blade.php
@@ -596,7 +596,7 @@ $url = url()->current();
 
 
             @if(auth()->user()->hasAnyPermission(['View blocked ip', 'View whitelisted ip']))
-                <li @if(preg_match('/report/i', $url)) class="active" @endif>
+                <li @if(preg_match('/ip/i', $url)) class="active" @endif>
                     <a href="#"><i class="fa fa-server"></i> <span class="nav-label">Block / Whitelist IPs</span><span
                             class="fa arrow"></span></a>
                     <ul class="nav nav-second-level collapse">

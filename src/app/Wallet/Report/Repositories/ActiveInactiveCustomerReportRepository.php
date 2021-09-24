078dfa2c87ccfeee471dae430ac8f2d44a628fb0
Shashwot <shashwot.shakya9@gmail.com>
2021-09-24 14:23:06 +0545

    active_inactive fix

--- src/app/Wallet/Report/Repositories/ActiveInactiveCustomerReportRepository.php
+++ src/app/Wallet/Report/Repositories/ActiveInactiveCustomerReportRepository.php
@@ -27,38 +27,35 @@ class ActiveInactiveCustomerReportRepository extends AbstractReportRepository
     public function __construct(Request $request)
     {
         parent::__construct($request);
-        $this->from = date('Y-m-d', strtotime(str_replace(',', ' ', $request->select_date)));
+        $this->from = date('Y-m-d H:i:s', strtotime(str_replace(',', ' ', $request->select_date)));
         $this->to = date('Y-m-d', strtotime(str_replace(',', ' ', $request->to_transaction_event)));
-        $this->sixMonthBeforeFromDate = Carbon::parse($this->from)->subMonths(6)->toDateString();
-        $this->twelveMonthBeforeFromDate = Carbon::parse($this->from)->subMonths(12)->toDateString();
+        $this->sixMonthBeforeFromDate = Carbon::parse($this->from)->subMonths(6)->toDateTimeString();
+        $this->twelveMonthBeforeFromDate = Carbon::parse($this->from)->subMonths(12)->toDateTimeString();
     }
 
     private function activeCustomerBuilder()
     {
+        $endOfFrom=Carbon::parse($this->from)->endOfDay()->toDateTimeString();
 //        $selectedDate= Carbon::parse($this->from)->endOfDay()->toDateString();
 //        dd($selectedDate);
-
         $activeUsers = \DB::connection('dpaisa')->select("SELECT * FROM (
             SELECT transaction_events.id as latest_transaction_id,transaction_events.user_id,transaction_events.created_at as latest_transaction_date,transaction_events.balance,transaction_events.bonus_balance FROM (
-        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$this->from') as transaction_in_range GROUP BY user_id
+        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$endOfFrom') as transaction_in_range GROUP BY user_id
         ) AS latest_transaction
         JOIN transaction_events ON transaction_events.id = latest_transaction.latest_transaction_id
-        WHERE latest_transaction.latest_transaction_date > '$this->sixMonthBeforeFromDate' AND latest_transaction.latest_transaction_date <='$this->from'
+        WHERE latest_transaction.latest_transaction_date >= '$this->sixMonthBeforeFromDate' AND latest_transaction.latest_transaction_date <='$endOfFrom'
 	) as latest_transaction_in_timeperiod
     RIGHT JOIN users
     ON users.id = latest_transaction_in_timeperiod.user_id
     WHERE (
         (
-            (users.phone_verified_at > '$this->sixMonthBeforeFromDate' AND users.phone_verified_at <= '$this->from')
+            (users.phone_verified_at >= '$this->sixMonthBeforeFromDate' AND users.phone_verified_at <= '$endOfFrom')
             AND
             NOT EXISTS(SELECT transaction_events.user_id,transaction_events.created_at
-    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$this->from'
+    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$endOfFrom'
         ))
             OR (latest_transaction_in_timeperiod.latest_transaction_id IS NOT NULL)
     	);");
-
-
-
 
         return $activeUsers;
 
@@ -66,22 +63,26 @@ class ActiveInactiveCustomerReportRepository extends AbstractReportRepository
 
     private function inactiveFor6to12MonthsCustomerBuilder()
     {
+        $endOfFrom=Carbon::parse($this->from)->endOfDay()->toDateTimeString();
+        $endOfSixMonths=Carbon::parse($this->sixMonthBeforeFromDate)->endOfDay()->toDateTimeString();
+
+
 
         $inactiveFor6to12MonthsUsers = \DB::connection('dpaisa')->select("SELECT * FROM (
             SELECT transaction_events.id as latest_transaction_id,transaction_events.user_id,transaction_events.created_at as latest_transaction_date,transaction_events.balance,transaction_events.bonus_balance FROM (
-        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$this->from') as transaction_in_range GROUP BY user_id
+        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$endOfFrom') as transaction_in_range GROUP BY user_id
         ) AS latest_transaction
         JOIN transaction_events ON transaction_events.id = latest_transaction.latest_transaction_id
-        WHERE latest_transaction.latest_transaction_date > '$this->twelveMonthBeforeFromDate' AND latest_transaction.latest_transaction_date <='$this->sixMonthBeforeFromDate'
+        WHERE latest_transaction.latest_transaction_date >= '$this->twelveMonthBeforeFromDate' AND latest_transaction.latest_transaction_date <='$endOfSixMonths'
 	) as latest_transaction_in_timeperiod
     RIGHT JOIN users
     ON users.id = latest_transaction_in_timeperiod.user_id
     WHERE (
         (
-            (users.phone_verified_at > '$this->twelveMonthBeforeFromDate' AND users.phone_verified_at <= '$this->sixMonthBeforeFromDate')
+            (users.phone_verified_at >= '$this->twelveMonthBeforeFromDate' AND users.phone_verified_at <= '$endOfSixMonths')
             AND
             NOT EXISTS(SELECT transaction_events.user_id,transaction_events.created_at
-    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$this->from'
+    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$endOfFrom'
         ))
             OR (latest_transaction_in_timeperiod.latest_transaction_id IS NOT NULL)
     	);");
@@ -92,22 +93,24 @@ class ActiveInactiveCustomerReportRepository extends AbstractReportRepository
 
     private function inactiveForMoreThan12MonthsCustomerBuilder()
     {
+        $endOfFrom=Carbon::parse($this->from)->endOfDay()->toDateTimeString();
+        $endOfTwelveMonths=Carbon::parse($this->twelveMonthBeforeFromDate)->endOfDay()->toDateTimeString();
 
         $inactiveForMoreThan12MonthsUsers = \DB::connection('dpaisa')->select("SELECT * FROM (
             SELECT transaction_events.id as latest_transaction_id,transaction_events.user_id,transaction_events.created_at as latest_transaction_date,transaction_events.balance,transaction_events.bonus_balance FROM (
-        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$this->from') as transaction_in_range GROUP BY user_id
+        SELECT MAX(id) as latest_transaction_id,user_id,MAX(created_at) as latest_transaction_date from (SELECT * FROM transaction_events WHERE created_at <= '$endOfFrom') as transaction_in_range GROUP BY user_id
         ) AS latest_transaction
         JOIN transaction_events ON transaction_events.id = latest_transaction.latest_transaction_id
-        WHERE latest_transaction.latest_transaction_date <='$this->twelveMonthBeforeFromDate'
+        WHERE latest_transaction.latest_transaction_date <='$endOfTwelveMonths'
 	) as latest_transaction_in_timeperiod
     RIGHT JOIN users
     ON users.id = latest_transaction_in_timeperiod.user_id
     WHERE (
         (
-            (users.phone_verified_at <= '$this->twelveMonthBeforeFromDate')
+            (users.phone_verified_at <= '$endOfTwelveMonths')
             AND
             NOT EXISTS(SELECT transaction_events.user_id,transaction_events.created_at
-    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$this->from'
+    FROM transaction_events WHERE users.id = transaction_events.user_id HAVING transaction_events.created_at <= '$endOfFrom'
         ))
             OR (latest_transaction_in_timeperiod.latest_transaction_id IS NOT NULL)
     	);");

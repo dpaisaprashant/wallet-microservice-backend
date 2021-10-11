040cf090f88c3c6c474bf1c6fab6536d5a701fe7
Avaya Baniya <baniyaavaya@gmail.com>
2021-10-11 12:47:48 +0545

    add lucky winner

---
+++ src/app/Http/Controllers/LuckyWinnerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\LoadTestFundEvent;
+use App\Models\LoadTestFund;
+use App\Models\TransactionEvent;
+use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class LuckyWinnerController extends Controller
+{
+    CONST VENDOR = "LUCKY WINNER";
+    CONST SERVICE_TYPE = "LUCKY WINNER";
+
+    public function index()
+    {
+        $transactionEventTransactionIds = TransactionEvent::where('service_type', self::SERVICE_TYPE)
+            ->where('transaction_type', LoadTestFund::class)
+            ->pluck("transaction_id");
+
+        $transactions = LoadTestFund::with('user')
+            ->whereIn('id', $transactionEventTransactionIds)
+            ->latest()
+            ->paginate(15);
+
+        return view('admin.luckyWinner.index')->with(compact('transactions'));
+    }
+
+    public function create(Request $request)
+    {
+        $users = User::latest()->get();
+        if ($request->isMethod('post')) {
+
+            $user = User::where('mobile_no', $request->mobile_no)->firstOrFail();
+
+
+            $currentBalance = Wallet::whereUserId($user->id)->first()->balance * 100;
+            $currentBonusBalance = Wallet::whereUserId($user->id)->first()->bonus_balance * 100;
+
+            if (empty($request['amount'])) $request['amount'] = 0;
+            if (empty($request['bonus_amount'])) $request['bonus_amount'] = 0;
+
+            Log::info("before_balance: " . $currentBalance);
+            Log::info("after_balance: " . ($currentBalance + ($request['amount'] * 100)));
+
+            $data = [
+                'admin_id' => auth()->user()->id,
+                'user_id' => $user->id,
+                'description' => $request['description'] ?? "Lucky winner for: " . today(),
+                'before_amount' => $currentBalance,
+                'after_amount' => $currentBalance + ($request['amount'] * 100),
+                //'after_amount' => $currentBalance + ($preTransaction->getOriginal('amount')),
+                'before_bonus_balance' => $currentBonusBalance,
+                'after_bonus_balance' => $currentBonusBalance + ($request['bonus_amount'] * 100)
+            ];
+
+            DB::beginTransaction();
+            try {
+                $transaction = LoadTestFund::create($data);
+                if (! $transaction) return redirect(route('luckyWinner.index'))->with('error', 'Transaction not created successfully');
+
+                event(new LoadTestFundEvent($transaction, self::VENDOR, self::SERVICE_TYPE));
+                DB::commit();
+                return redirect(route('luckyWinner.index'))->with('success', 'Transaction created successfully');
+            } catch (\Exception $e) {
+                Log::info($e);
+                DB::rollBack();
+                return redirect(route('luckyWinner.index'))->with('error', 'Transaction not created successfully');
+            }
+
+        }
+
+        return view('admin.luckyWinner.create')->with(compact('users'));
+    }
+}

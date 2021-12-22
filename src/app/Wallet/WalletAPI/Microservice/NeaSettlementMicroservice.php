3f80ab5b586d129f1fbff8775456852de2cd1a43
subhay99 <sthapitsubhay99@gmail.com>
2021-12-22 14:28:25 +0545

    nea-settlement

---
+++ src/app/Wallet/WalletAPI/Microservice/NeaSettlementMicroservice.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Wallet\WalletAPI\Microservice;
+
+use Illuminate\Http\Request;
+
+class NeaSettlementMicroservice
+{
+    public function SettleNea(Request $request){
+
+    }
+}

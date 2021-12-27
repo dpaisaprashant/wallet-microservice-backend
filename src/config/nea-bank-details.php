daeea3e3503f6cb8fd3f01d5b0f5aafbdd658f0b
Avaya Baniya <baniyaavaya@gmail.com>
2021-12-27 17:53:52 +0545

    update config

--- src/config/nea-bank-details.php
+++ src/config/nea-bank-details.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    399 => ['bank_id'=>0301,'bank_name'=>"Agricultural Development Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"1006601473391017",'account_name'=>"NEA Barahathawa Collection cen"],
-    286 => ['bank_id'=>3001,'bank_name'=>"Civil Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"00510032637197",'account_name'=>"NEA-Birgunj"],
-    229 => ['bank_id'=>3001,'bank_name'=>"Civil Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"01910032637183",'account_name'=>"NEA-Butwal"],
-    217 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"0330014503501287",'account_name'=>"NEA-Chapagaun"],
-    281 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"1180014503501582",'account_name'=>"NEA-Inarwa"],
-    261 => ['bank_id'=>2501,'bank_name'=>"NMB Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"0300001460000011",'account_name'=>"NEA CASH COLLECTION A/C JANAKPUR"],
-    379 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"1230014503500970",'account_name'=>"NEA-Lalbandi"],
-    207 => ['bank_id'=>0301,'bank_name'=>"Agricultural Development Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"0212001473391011",'account_name'=>"NEA Pulchowk Collection Center"],
-    312 => ['bank_id'=>2301,'bank_name'=>"NIC Asia Bank Limited",'branch_id'=>null,'branch_name'=>"Head Office",'account_number'=>"0524150055292002",'account_name'=>"Nepal Electricity Authority-Siraha"],
+    399 => ['bank_id'=>0301,'bank_name'=>"Agricultural Development Bank Limited",'branch_id'=>2,'branch_name'=>"Head Office",'account_number'=>"1006601473391017",'account_name'=>"NEA Barahathawa Collection cen"],
+    286 => ['bank_id'=>3001,'bank_name'=>"Civil Bank Limited",'branch_id'=>1,'branch_name'=>"Head Office",'account_number'=>"00510032637197",'account_name'=>"NEA-Birgunj"],
+    229 => ['bank_id'=>3001,'bank_name'=>"Civil Bank Limited",'branch_id'=>1,'branch_name'=>"Head Office",'account_number'=>"01910032637183",'account_name'=>"NEA-Butwal"],
+    217 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>1,'branch_name'=>"Head Office",'account_number'=>"0330014503501287",'account_name'=>"NEA-Chapagaun"],
+    281 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>1,'branch_name'=>"Head Office",'account_number'=>"1180014503501582",'account_name'=>"NEA-Inarwa"],
+    261 => ['bank_id'=>2501,'bank_name'=>"NMB Bank Limited",'branch_id'=>9,'branch_name'=>"Head Office",'account_number'=>"0300001460000011",'account_name'=>"NEA CASH COLLECTION A/C JANAKPUR"],
+    379 => ['bank_id'=>1501,'bank_name'=>"Machhapuchhre Bank Limited",'branch_id'=>1,'branch_name'=>"Head Office",'account_number'=>"1230014503500970",'account_name'=>"NEA-Lalbandi"],
+    207 => ['bank_id'=>0301,'bank_name'=>"Agricultural Development Bank Limited",'branch_id'=>2,'branch_name'=>"Head Office",'account_number'=>"0212001473391011",'account_name'=>"NEA Pulchowk Collection Center"],
+    312 => ['bank_id'=>2301,'bank_name'=>"NIC Asia Bank Limited",'branch_id'=>32,'branch_name'=>"Head Office",'account_number'=>"0524150055292002",'account_name'=>"Nepal Electricity Authority-Siraha"],
 
 ];

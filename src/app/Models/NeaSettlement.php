5b28975de14ef4230fb6c0b54b728652b765fdfa
Avaya Baniya <baniyaavaya@gmail.com>
2021-12-27 16:15:31 +0545

    add log

--- src/app/Models/NeaSettlement.php
+++ src/app/Models/NeaSettlement.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class NeaSettlement extends Model
 {
+    use LogsActivity;
+
+    protected static $logName = 'NEA Settlement';
+    protected static $logAttributes = ['*'];
 //    public function bankTransfer(){
 //        $this->belongsTo(NonRealTimeBankTransfer::class);
 //    }

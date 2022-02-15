907d351e67a89bbdb4b4f2ad2488950e74a222e1
Shashwot <shashwot.shakya9@gmail.com>
2022-02-15 13:35:58 +0545

    nrb report update

---
+++ src/database/migrations/2022_02_14_135517_alter_pre_transactions_table_add_company_code_column.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterPreTransactionsTableAddCompanyCodeColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        if (Schema::connection('dpaisa')->hasTable("pre_transactions")) {
+
+            if (! Schema::connection('dpaisa')->hasColumn("pre_transactions", "company_code")) {
+                Schema::connection('dpaisa')->table('pre_transactions', function (Blueprint $table) {
+                    $table->integer('company_code')->nullable();
+                });
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

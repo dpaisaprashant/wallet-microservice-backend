ebae87a1e24a600049121f91ed28299a46754fad
Shashwot <shashwot.shakya9@gmail.com>
2022-02-15 13:08:33 +0545

    service type seeder added

---
+++ src/database/seeds/ServiceTypeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
+class ServiceTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //add cashback for agent types that do not exist
+
+    }
+}

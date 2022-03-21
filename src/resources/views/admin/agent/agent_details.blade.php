0a7792353a702a2243d2be61f00b5118107cf15b
Avaya Baniya <baniyaavaya@gmail.com>
2022-03-21 17:36:02 +0545

    agent code backend

--- src/resources/views/admin/agent/agent_details.blade.php
+++ src/resources/views/admin/agent/agent_details.blade.php
@@ -265,7 +265,7 @@
                                         <td>{{$user->kyc->ward_no ?? null}}</td>
                                         <td>
                                             @if($user->wallet)
-                                                Rs. {{ $user->walllet->balance + $user->wallet->bonus_balance }}
+                                                Rs. {{ optional($user->walllet)->balance + optional($user->wallet)->bonus_balance }}
                                             @else
                                                 Rs. 00
                                             @endif

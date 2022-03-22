a2d81fdc1fd2c0ed221d42897dbf2f86fc4d93ae
erp1 <nitish.atom@gmail.com>
2022-03-22 17:56:58 +0545

    service code

--- src/database/seeds/CompanyServiceCodeSeeder.php
+++ src/database/seeds/CompanyServiceCodeSeeder.php
@@ -8,21 +8,26 @@ class CompanyServiceCodeSeeder extends Seeder
 {
     public function run()
     {
-        $preTransactions = PreTransaction::where('vendor', 'PAYPOINT')->orderBy('created_at', 'DESC')->get();
+        for ($i = 20000; $i <= 180000; $i = $i + 10000) {
+            $preTransactions = PreTransaction::where('vendor', 'PAYPOINT')->orderBy('created_at', 'DESC')
+                ->limit(10000)
+                ->skip($i)
+                ->get();
 
-        foreach ($preTransactions as $preTransaction) {
-            $jsonRequest = json_decode($preTransaction->json_request, true);
+            foreach ($preTransactions as $preTransaction) {
+                $jsonRequest = json_decode($preTransaction->json_request, true);
 
-            if ($jsonRequest['company_code'] !== null) {
-                $preTransaction->update(["company_code" => $jsonRequest['company_code']]);
+                if ($jsonRequest['company_code'] !== null) {
+                    $preTransaction->update(["company_code" => $jsonRequest['company_code']]);
+                }
+
+                if ($jsonRequest['service_code'] !== null) {
+                    if ($jsonRequest['service_code'] === 0) {
+                        $preTransaction->update(["service_code" => 0]);
+                    }
+                    $preTransaction->update(["service_code" => $jsonRequest['service_code']]);
+                }
             }
-
-            if ($jsonRequest['service_code'] !== null) {
-                if($jsonRequest['service_code'] === 0){
-                    $preTransaction->update(["service_code" => 0]);
-            }
-            $preTransaction->update(["service_code" => $jsonRequest['service_code']]);
         }
     }
-}
 }

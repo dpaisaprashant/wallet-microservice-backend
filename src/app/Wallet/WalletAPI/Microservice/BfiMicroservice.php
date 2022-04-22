03c3c8bf414ce4da438089fd529977f89f1ef5b3
Avaya Baniya <baniyaavaya@gmail.com>
2022-04-22 15:25:33 +0545

    add log

--- src/app/Wallet/WalletAPI/Microservice/BfiMicroservice.php
+++ src/app/Wallet/WalletAPI/Microservice/BfiMicroservice.php
@@ -5,6 +5,7 @@ namespace App\Wallet\WalletAPI\Microservice;
 use App\Wallet\WalletAPI\BackendWalletAPIMicroservice;
 use App\Wallet\WalletAPI\MicroserviceJSONAbstract;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class BfiMicroservice
 {
@@ -12,6 +13,10 @@ class BfiMicroservice
     {
         $username = config('bfi-users.'.$request->bfi_user.'username');
         $password = config('bfi-users.'.$request->bfi_user.'password');
+
+        Log::info("Bfi username: " . $username);
+        Log::info("Bfi password: " . $password);
+
         $basicAuth = base64_encode($username.':'.$password);
 
         $microservice = new MicroserviceJSONAbstract();
